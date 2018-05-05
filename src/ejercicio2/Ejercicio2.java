e9a7d2199bb0c512c62a1f69e47c4e86bce0895d
aamaya11 <39015108+aamaya11@users.noreply.github.com>
2018-05-05 17:33:32 -0600

    Add files via upload

---
+++ src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,17 @@
+
+package ejercicio2;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
